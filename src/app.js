5e0e7215daf65a409a420874e3197e0565f5639b
Alejandro Tapia <78757063+AlejandroSTapia@users.noreply.github.com>
2026-08-16 19:37:21 -0600

    Fix swagger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import textRouter from './routes/textRoutes.js';
-import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './config/swagger.js';
 
 const app = express();
@@ -9,6 +8,41 @@ app.use(express.json());
 
 app.use('/text', textRouter);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get('/api-docs/swagger.json', (req, res) => {
+  res.json(swaggerSpec);
+});
+
+app.get(['/api-docs', '/api-docs/'], (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="es">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>API Documentation</title>
+
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/swagger-ui-dist@5.32.0/swagger-ui.css"
+        />
+      </head>
+
+      <body>
+        <div id="swagger-ui"></div>
+
+        <script src="https://unpkg.com/swagger-ui-dist@5.32.0/swagger-ui-bundle.js"></script>
+
+        <script>
+          window.onload = () => {
+            SwaggerUIBundle({
+              url: '/api-docs/swagger.json',
+              dom_id: '#swagger-ui'
+            });
+          };
+        </script>
+      </body>
+    </html>
+  `);
+});
 
 export default app;
